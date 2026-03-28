c5b5f1bc193e3363bdd993532cd7c0797cbdf264
Khamit <admiral3nelson7@gmail.com>
2026-03-28 11:31:06 +0500

    init

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -4,12 +4,10 @@ export default async function LangHomePage({
 	params: Promise<{ lang: string }>
 }) {
 	const { lang } = await params
-
 	return (
 		<main style={{ padding: '2rem' }}>
-			<h1>SalyqApp</h1>
+			<h1>SalyqApp ✅</h1>
 			<p>Язык: {lang}</p>
-			<p>Проект запущен ✅</p>
 		</main>
 	)
 }

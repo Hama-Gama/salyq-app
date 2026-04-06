a02c06a9af57a12e10390a93b51c51f95e155995
Khamit <115399990+Hama-Gama@users.noreply.github.com>
2026-04-06 09:57:47 +0500

    Update layout file with new metadata and layout function

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,5 @@ export default function RootLayout({
 }: {
 	children: React.ReactNode
 }) {
-	return (
-		<html suppressHydrationWarning>
-			<body>{children}</body>
-		</html>
-	)
+	return children
 }
